625983fb98343b581b919368fd379682bb03c7ee
Mingyu Han <133757639+aitchhhan@users.noreply.github.com>
2025-06-03 21:43:38 +0900

    [Bronze IV] Title: 숫자의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.cc
@@ -1,14 +1,17 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
     int n;
-    string s;
+    char c[101];
     int sum = 0;
-    cin >> n >> s;
 
-    for(int i = 0; i < n; i++) {
-        sum += (s[i] - '0');
+    cin >> n >> c;
+
+    for (int i = 0; i < n; i++) {
+        char temp[2] = { c[i], '\0'};
+        sum += atoi(temp);
     }
     cout << sum;
 }
