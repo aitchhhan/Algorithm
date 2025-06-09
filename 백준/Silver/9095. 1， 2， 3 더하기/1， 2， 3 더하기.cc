b8cb9830f45ac12d7e26d5b8367e276f45f22842
Mingyu Han <133757639+aitchhhan@users.noreply.github.com>
2025-06-09 21:59:04 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.cc
@@ -2,28 +2,20 @@
 #include <vector>
 using namespace std;
 
-vector<int> arr(12, -1);
-
-int sum(int num) {
-    if (num == 1)
-        return 1;
-    if (num == 2)
-        return 2;
-    if (num == 3)
-        return 4;
-    if (arr[num] != -1)
-        return arr[num];
-    return arr[num] = sum(num - 1) + sum(num - 2) + sum(num - 3);
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
     int T, n;
     cin >> T;
-    while (T--) {
+    vector<int> dp(12, 0);
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i < 12; i++)
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    for (int k = 0; k < T; k++) {
         cin >> n;
-        cout << sum(n) << '\n';
+        cout << dp[n] << '\n';
     }
 }
